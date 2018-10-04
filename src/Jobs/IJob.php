b202923c16a69b137df4943bb18dc6c87ba9e63b
sophiegauthier <elait.sa1@gmail.com>
2018-10-04 14:53:38 +0100

    Cleanup

--- src/Jobs/IJob.php
+++ src/Jobs/IJob.php
@@ -7,7 +7,7 @@ interface IJob
     function serialise(): string;
 
     function deserialise(string $data): void;
-//
+
     /**
      * This should return a status number as defined in {Job::class}
      * @return int
