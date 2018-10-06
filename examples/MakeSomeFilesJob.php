ca8d6a14711dd6a216e9355afaae4be0428defa3
sophiegauthier <elait.sa1@gmail.com>
2018-10-06 15:42:59 +0100

    Updated basic example

--- examples/MakeSomeFilesJob.php
+++ examples/MakeSomeFilesJob.php
@@ -8,10 +8,14 @@ class MakeSomeFilesJob extends \PHQ\Jobs\Job{
      */
     function run(): int
     {
+        //You can access the raw payload by calling $this->data->getPayload();
+        //However using this method you can ensure that the dataset the job is passed is correct.
         $payload = $this->getPayload(MakeSomeFilesPayload::class);
 
+        //TODO implement job!
         var_dump($payload);
 
+        //Return a status to tell the storage handler what to update the status to.
         return \PHQ\Jobs\Job::STATUS_SUCCESS;
     }
 }
--- examples/basic.php
+++ examples/basic.php
@@ -5,16 +5,20 @@ require_once __DIR__ . "/MakeSomeFilesJob.php";
 require_once __DIR__ . "/MakeSomeFilesPayload.php";
 
 $phq = new \PHQ\PHQ();
-
 $payload = new MakeSomeFilesPayload();
 
+//Setting up the payload that will be used when the job is run
 $payload->setDir("./files");
 $payload->setFileCount(10);
 
+//Create the job instance and set the payload
 $job = new MakeSomeFilesJob($payload);
 
+//Add the job to the queue, this will use the configured storage handler to save all the job data
 $phq->enqueue($job);
 
+//Retrieve the next job off the queue using the storage handler
 $job = $phq->getNext();
 
+//Run the job
 $job->run();
